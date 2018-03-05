e294222a5071d6e425e8011fa08d9c1ec9ee45ec
Jonas Johan Solsvik <jonas.solsvik@gmail.com>
2018-03-05 21:10:12 +0100

    more beutiful

--- main.cpp
+++ main.cpp
@@ -17,18 +17,19 @@ enum CommandCharacter : char {
 };
 
 struct Command {
-    using HandlerType = bool (*)(const Command&);
+    using ActionType = bool (*)(const Command&);
 
     const char character{};
-    const HandlerType handler;  // @doc implicitly cast lamba to c-function pointer - https://stackoverflow.com/a/23616995 - 05.03.18
+    const ActionType action;  // @doc implicitly cast lamba to c-function pointer - https://stackoverflow.com/a/23616995 - 05.03.18
     const std::string textHelp{};
     const std::string textSuccess{};
+    const std::string textExit{};
     const std::string textFailure{};
 
     Command() = default;
 };
 
-namespace CommandHandler {
+namespace Action {
 auto registrerNyResept        = [](const Command&) -> bool { return true; };
 auto lagOversiktReseptLege    = [](const Command&) -> bool { return true; };
 auto lagOversiktReseptPasient = [](const Command&) -> bool { return true; };
@@ -37,10 +38,7 @@ auto fjernGamleResepter       = [](const Command&) -> bool { return true; };
 auto skrivTilFil              = [](const Command&) -> bool { return true; };
 auto lesInnFraFil             = [](const Command&) -> bool { return true; };
 auto hjelp                    = [](const Command&) -> bool { return true; };
-auto quit                     = [](const Command&) -> bool { 
-
-    return false; 
-};
+auto quit                     = [](const Command&) -> bool { return false; };
 }
 
 namespace jet {
@@ -52,29 +50,30 @@ constexpr auto printline = [](auto string) {
 int main() {
 
     const std::vector<Command> validCommands = {
-        {REGISTRER,        CommandHandler::registrerNyResept,        "Registrer en ny resept", "Ny resept registert", "Registrering av resept mislykket, venligst prøv igjen.."},
-        {LAG_OVERSIKT,     CommandHandler::lagOversiktReseptLege,    "Lag oversikt over reseptene fra en bestemt lege", "Viser oversikt", "Ingenting å vise"}, 
-        {PASIENT_OVERSIKT, CommandHandler::lagOversiktReseptPasient, "Lag oversikt over resepter til en bestemt pasient", "Viser oversikt", "Ingenting å vise"}, 
-        {MEDISIN_OVERSIKT, CommandHandler::lagOversiktMedisinBruk,   "Lag oversikt over bruk av bestemte medisiner", "Viser oversikt", "Ingenting å vise"},
-        {FJERN,            CommandHandler::fjernGamleResepter,       "Fjern alle gamle resepter", "Fjernet alle gamle resepter", "Fjernet ingen resepter"},
-        {UT_TIL_FIL,       CommandHandler::skrivTilFil,              "Skriv ut til fil", "Skriving til fil gikk bra", "Skriving til fil var mislykket. Venligst prøv igjen.."},
-        {INN_FRA_FIL,      CommandHandler::lesInnFraFil,             "Les inn fra fil", "Lesing fra fil gikk bra", "Lesing fra fil var mislykket. Venligst prøv igjen.."},
-        {HJELP,            CommandHandler::hjelp,                    "Hjelp"},
-        {QUIT,             CommandHandler::quit,                     "Quit",  "", "Quitting..."}
+        {REGISTRER,        Action::registrerNyResept,        "Registrer en ny resept", "Ny resept registert", "Registrering av resept mislykket, venligst prøv igjen.."},
+        {LAG_OVERSIKT,     Action::lagOversiktReseptLege,    "Lag oversikt over reseptene fra en bestemt lege", "Viser oversikt", "Ingenting å vise"}, 
+        {PASIENT_OVERSIKT, Action::lagOversiktReseptPasient, "Lag oversikt over resepter til en bestemt pasient", "Viser oversikt", "Ingenting å vise"}, 
+        {MEDISIN_OVERSIKT, Action::lagOversiktMedisinBruk,   "Lag oversikt over bruk av bestemte medisiner", "Viser oversikt", "Ingenting å vise"},
+        {FJERN,            Action::fjernGamleResepter,       "Fjern alle gamle resepter", "Fjernet alle gamle resepter", "Fjernet ingen resepter"},
+        {UT_TIL_FIL,       Action::skrivTilFil,              "Skriv ut til fil", "Skriving til fil gikk bra", "Skriving til fil var mislykket. Avslutter applikasjon..."},
+        {INN_FRA_FIL,      Action::lesInnFraFil,             "Les inn fra fil", "Lesing fra fil gikk bra", "Lesing fra fil var mislykket. Avslutter applikasjon..."},
+        {HJELP,            Action::hjelp,                    "Hjelp", "Skriver ut liste med kommandoer"},
+        {QUIT,             Action::quit,                     "Quit",  "", "Quitting..."}
     };
 
-    auto printValidCommands = [&validCommands]() {
-        jet::printline("\nCommand list:          ");
-        jet::printline("-------------------------");
-        for (const auto c : validCommands) {
-            jet::printline(" " + std::string(1, c.character) + " : " + c.textHelp);
-        }
-    };
+    const auto readValidCommand = [&validCommands]() -> Command {
+        
+        auto printValidCommands = [&validCommands]() {
+            jet::printline("\nCommand list:          ");
+            jet::printline("-------------------------");
+            for (const auto& c : validCommands) {
+                jet::printline(" " + std::string(1, c.character) + " : " + c.textHelp);
+            }
+        };
 
-    auto readValidCommand = [&validCommands, &printValidCommands]() -> Command {
         std::string input{};
+        printValidCommands();
         for (;;) {
-            printValidCommands();
 
             std::getline(std::cin, input);
             // 1. Check if length is correct
@@ -91,15 +90,17 @@ int main() {
         }
     };
 
-    bool success = true;
-    while(success) {
+    for(;;) {
         Command command = readValidCommand();
 
         jet::printline("Running " + command.textHelp);        
-        success = command.handler(command);
         
-        if(success) jet::printline(command.textSuccess);
-        else        jet::printline(command.textFailure);        
+        if(command.action(command)) { 
+            jet::printline(command.textSuccess);
+        } else {        
+            jet::printline(command.textExit);        
+            break;
+        }
     }
 
     return 0;
