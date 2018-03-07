991931725fce1a2a558c8b37d2a95ce7f7483200
Jonas Johan Solsvik <jonas.solsvik@gmail.com>
2018-03-07 02:55:41 +0100

    implemented l,p,u,i,h,q almost r but lacking validation, no F and M

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <cctype>
 #include <cstdint>
+#include <algorithm>
 
 #include "ListTool2a.h"
 
@@ -44,18 +45,22 @@ public:
         auto operator++(int) -> iterator              { node = node->next;  return *this;  }
         bool operator!=(const iterator& other) const  { return node != other.node; }
         bool operator==(const iterator& other) const  { return node == other.node;  }
-        auto operator*() -> ElementType&              { return *(ElementType*)node->listElement; }
-        auto operator*() const -> const ElementType&  { return *(ElementType*)node->listElement; }
+        auto operator*() -> ElementType*              { return (ElementType*)node->listElement; }
+        auto operator*() const -> const ElementType*  { return (ElementType*)node->listElement; }
         auto operator->() -> ElementType*             { return (ElementType*)node->listElement;  }
         auto operator->() const -> const ElementType* { return (ElementType*)node->listElement;  }
     };
     Listx() = delete;
-    Listx(ListType li): List(li) { std::cout << "Created ListEx\n"; }
+    Listx(ListType li): List(li) { }
     
-    auto begin() const -> iterator { return iterator{ this->first }; }
+    auto begin() const -> iterator { return iterator{ this->first->next }; }
     auto end()   const -> iterator { return iterator{ this->last }; }
 };
 
+//
+// @function jet::printline()
+//  @brief A wrapper for std::cout which adds a \n at the end of each print.
+//
 template<class Arg>
 constexpr void printline(Arg arg) {  
     std::cout << arg << '\n';    
@@ -66,21 +71,54 @@ constexpr void printline(Arg arg, Args ... args) {
     std::cout << arg << ' ';
     printline(std::forward<Args>(args)...);  
 }
+
+constexpr auto getline = [](const auto msg) -> std::string {
+    std::string line{};
+    std::cout << msg << " : ";
+    std::getline(std::cin, line);
+    return line;
+};
+
+constexpr auto getnumber = [](const auto msg) -> uint64 {
+    uint64 number;
+    std::cout << msg << " : ";
+    std::cin >> number;
+    std::cin.ignore();
+    return number;
+};
+
+constexpr auto readline = [](auto& inn) -> std::string {
+    std::string line{};
+    std::getline(inn, line);
+    return line;
+};
+
+constexpr auto readnumber = [](auto& inn) {
+    uint64 number{0};
+    inn >> number; inn.ignore();
+    return number;
+};
+
+constexpr auto writeline = [](auto& out, const auto& data){
+    out << data << '\n';
+};
+
 } // JET END
 
 struct Context {
+
     struct Resept : TextElement {
 
         using string = std::string;
         using uint64 = std::uint64_t;        
         
-        string dato;
-        string pasientNavn;
-        string legeNavn;
-        string legeAdresse;
-        string legeTelefon;
-        string medisinNavn;
-        uint64 medisinMengde; // milligram, mg
+        string dato{};
+        string pasientNavn{};
+        string legeNavn{};
+        string legeAdresse{};
+        string legeTelefon{};
+        string medisinNavn{};
+        uint64 medisinMengde{}; // milligram, mg
 
         Resept(): TextElement("default") {}
         Resept(string _dato,
@@ -99,6 +137,16 @@ struct Context {
         ,medisinNavn(_medisinNavn)
         ,medisinMengde(_medisinMengde)
         {}
+
+        void display() const {
+            JET::printline("Dato " + dato);
+            JET::printline("Pasientnavn " + pasientNavn);
+            JET::printline("Legenavn " + legeNavn);
+            JET::printline("Legeadresse " + legeAdresse);
+            JET::printline("Legetelefon " + legeTelefon);
+            JET::printline("Medisin " + medisinNavn);
+            JET::printline("Medisin mg " + std::to_string(medisinMengde));
+        }
     };
     JET::Listx<Resept> resepter{Sorted}; 
 };
@@ -136,7 +184,7 @@ struct Command {
     string textHelp{};
     string textStatus{};
 
-    auto status(ReturnCode code, const char* statusMsg) -> ReturnCode {
+    auto status(ReturnCode code, const std::string&& statusMsg) -> ReturnCode {
         textStatus = statusMsg;
         return code;
     }
@@ -144,42 +192,94 @@ struct Command {
 
 
 namespace Action {
+
+//
+// @function
+//
 auto registrerNyResept(Command& cmd, Context& ctx) -> Command::ReturnCode { 
 
-    constexpr auto getline = [](const auto msg) -> std::string {
-        std::string line{};
-        std::cout << msg << " : ";
-        std::getline(std::cin, line);
-        return line;
-    };
-
-    constexpr auto getnumber = [](const auto msg) -> uint64 {
-        uint64 number;
-        std::cout << msg << " : ";
-        std::cin >> number;
-        return number;
-    };
-
     auto resept = new Context::Resept {
-        getline("Dato (ÅÅÅÅMMDD)"),
-        getline("Pasient navn  (Jonas Solsvik)"),
-        getline("Lege navn     (-------------)"),
-        getline("Lege addresse (Snorres veg)  "),
-        getline("Lege telefon  (45 20 08 77)  "),
-        getline("Medisin navn                 "),
-        getnumber("Medisin mengde i milligram")   
+        JET::getline("Dato (ÅÅÅÅMMDD)"),
+        JET::getline("Pasient navn  (Jonas Solsvik)"),
+        JET::getline("Lege navn     (-------------)"),
+        JET::getline("Lege addresse (Snorres veg)  "),
+        JET::getline("Lege telefon  (45 20 08 77)  "),
+        JET::getline("Medisin navn                 "),
+        JET::getnumber("Medisin mengde i milligram")   
     };
 
     ctx.resepter.add(resept);
     return cmd.status(Command::CONTINUE, "New resept added"); 
 };
 
+//
+// @function
+//
+auto lagOversiktReseptLege(Command& cmd, Context& ctx) -> Command::ReturnCode { 
+    if (ctx.resepter.isEmpty()) {
+        return cmd.status(Command::CONTINUE, "No resepts found.");
+    }
+    auto _legeNavn = JET::getline("\nLegenavn:");
+    
+    for (const Context::Resept* resept : ctx.resepter) {
 
-auto lagOversiktReseptLege(Command& cmd, Context& ctx) -> Command::ReturnCode { return Command::CONTINUE; };
-auto lagOversiktReseptPasient(Command& cmd, Context& ctx) -> Command::ReturnCode { return Command::CONTINUE; };
-auto lagOversiktMedisinBruk(Command& cmd, Context& ctx) -> Command::ReturnCode { return Command::CONTINUE; };
-auto fjernGamleResepter(Command& cmd, Context& ctx) -> Command::ReturnCode { return Command::CONTINUE; };
+        if (resept->legeNavn == _legeNavn) {
+            JET::printline("-----------------");        
+            resept->display();
+        }
+    }
+    JET::printline("-----------------");        
+    return Command::CONTINUE; 
+};
 
+//
+// @function
+//
+auto lagOversiktReseptPasient(Command& cmd, Context& ctx) -> Command::ReturnCode {
+
+    if (ctx.resepter.isEmpty()) {
+        return cmd.status(Command::CONTINUE, "No resepts found.");
+    }
+    auto _pasientNavn = JET::getline("\nPasientnavn:");
+    
+    for (const Context::Resept* resept : ctx.resepter) {
+        if (resept->pasientNavn == _pasientNavn) {
+            JET::printline("-----------------");                    
+            resept->display();
+        }
+    }
+    JET::printline("-----------------");        
+    return Command::CONTINUE;  
+};
+
+//
+// @function
+//
+auto lagOversiktMedisinBruk(Command& cmd, Context& ctx) -> Command::ReturnCode { 
+    
+    if (ctx.resepter.isEmpty()) {
+        return cmd.status(Command::CONTINUE, "No resepts found.");
+    }
+    
+    return Command::CONTINUE; 
+    
+};
+
+//
+// @function
+//
+auto fjernGamleResepter(Command& cmd, Context& ctx) -> Command::ReturnCode { 
+    
+    if (ctx.resepter.isEmpty()) {
+        return cmd.status(Command::CONTINUE, "No resepts found.");
+    }
+    
+    return Command::CONTINUE; 
+};
+
+//
+// @function
+//
 auto lesFraFil(Command& cmd, Context& ctx) -> Command::ReturnCode
 { 
     std::ifstream innfil(DATAFILE, std::ios::binary);                                         
@@ -187,43 +287,78 @@ auto lesFraFil(Command& cmd, Context& ctx) -> Command::ReturnCode
         return cmd.status(Command::EXIT, "Datafile not exist. Could not read. Please create resept.dta file and restart the app. Exiting!");
     }
 
-    innfil.seekg (0, innfil.end);
-
-    uint32 length = innfil.tellg();
-    uint32 reseptCount = length/sizeof(*(ctx.resepter.begin()));
-
-    // Is file empty
+    uint32 reseptCount = JET::readnumber(innfil);
     if (reseptCount == 0) {
-        return cmd.status(Command::CONTINUE, "The file innfile is empty. No resepts fround.");
+        return cmd.status(Command::CONTINUE, "The file innfile is empty. Nothing to read.");
     }
 
-    JET::printline("Fant", reseptCount, "resepter på fil!");
-    innfil.seekg (0, innfil.beg);
-    
+    // Clear list
+    using namespace JET;
+    while(ctx.resepter.noOfElements() > 0) ctx.resepter.removeNo(ctx.resepter.noOfElements());
+
+
     for (uint32 i = 0; i < reseptCount; ++i) {
-        auto resept = new Context::Resept{};
-        innfil.read((char*) resept, sizeof(Context::Resept));
+
+        auto resept = new Context::Resept {
+            JET::readline(innfil), // dato{};
+            JET::readline(innfil), // pasientNavn{};
+            JET::readline(innfil), // legeNavn{};
+            JET::readline(innfil), // legeAdresse{};
+            JET::readline(innfil), // legeTelefon{};
+            JET::readline(innfil), // medisinNavn{};
+            JET::readnumber(innfil)// uint64 medisinMengde{}; //
+        };
         ctx.resepter.add(resept);
     } 
-
-    return cmd.status(Command::CONTINUE, "Finished reading file.");
+    return cmd.status(Command::CONTINUE, ("Finished reading " + std::to_string(reseptCount) + " resepts"));
 }
 
-
+//
+// @function
+//
 auto skrivTilFil(Command& cmd, Context& ctx) -> Command::ReturnCode { 
+
     std::ofstream utfil(DATAFILE, std::ios::binary);
+
+    if (ctx.resepter.isEmpty()) {
+        return cmd.status(Command::CONTINUE, "You have no resepts. Nothing to write to file");
+    }
+
     if (!utfil) {
         return cmd.status(Command::EXIT, "Datafile not exist. Could not write. Please create file resept.dta and restart the app. EXITING!");
     }  
-    
-    for(const auto& resept : ctx.resepter) {
-        utfil.write((char*) &resept, sizeof(Context::Resept));
+
+    JET::writeline(utfil, ctx.resepter.noOfElements());
+    for (const Context::Resept* resept : ctx.resepter) {
+        JET::writeline(utfil, resept->dato);
+        JET::writeline(utfil, resept->pasientNavn);
+        JET::writeline(utfil, resept->legeNavn);
+        JET::writeline(utfil, resept->legeAdresse);
+        JET::writeline(utfil, resept->legeTelefon);
+        JET::writeline(utfil, resept->medisinNavn);
+        JET::writeline(utfil, resept->medisinMengde);
     }
-    return cmd.status(Command::CONTINUE, "Finished writing to file");
+    return cmd.status(Command::CONTINUE, "Finished writing " + std::to_string(ctx.resepter.noOfElements()) + " to file.");
 }
 
-auto hjelp(Command& cmd, Context& ctx) -> Command::ReturnCode { return cmd.status(Command::CONTINUE, "Showing valid commands"); };
-auto quit(Command& cmd, Context& ctx) -> Command::ReturnCode { return cmd.status(Command::EXIT, "Quitting app..."); };
+
+//
+// @function
+//
+auto hjelp(Command& cmd, Context& ctx) -> Command::ReturnCode 
+{ 
+    return cmd.status(Command::CONTINUE, "Showing valid commands"); 
+};
+
+
+//
+// @function
+//
+auto quit(Command& cmd, Context& ctx) -> Command::ReturnCode 
+{ 
+    return cmd.status(Command::EXIT, "Quitting app..."); 
+};
+
 } // ACTION END
 
 
@@ -297,10 +432,11 @@ int main() {
         };
 
         auto command = readValidCommand(validCommands);
-        JET::printline("Running", command.textHelp);        
-        
+        JET::printline("--------------------------------------------");
+        JET::printline(command.textHelp);                
         auto status = command.action(command, ctx);
         JET::printline(command.textStatus);
+        JET::printline("--------------------------------------------");
 
         if (status == Command::EXIT)
             break;
